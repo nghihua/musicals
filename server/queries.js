e065cf32abd63d63f55b4ca70ef4ae85e954a955
Nghi <huanhatgianghi@gmail.com>
2021-11-07 23:54:20 +0700

    use DATABASE env variable

--- server/queries.js
+++ server/queries.js
@@ -1,13 +1,13 @@
 const pg = require('pg');
 
 const Pool = require('pg').Pool
-const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'api',
-  password: 'password',
-  port: 5432,
-})
+
+const pool = new Pool({ // create connection to database
+  connectionString: process.env.DATABASE_URL,	// use DATABASE_URL environment variable from Heroku app 
+  ssl: {
+    rejectUnauthorized: false // don't check for SSL cert
+  }
+});
 
 const getMusicals = (request, response) => {
   console.log('Get musical by key!');
