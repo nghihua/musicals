54ca35012fd5b2e5b3c5995655afcd1938fce8c6
Nghi <huanhatgianghi@gmail.com>
2021-11-08 16:58:11 +0700

    add Procfile

--- queries.js
+++ queries.js
@@ -2,20 +2,20 @@ const pg = require('pg');
 
 const Pool = require('pg').Pool
 
-const pool = new Pool({ // create connection to database
+/* const pool = new Pool({ // create connection to database
   connectionString: process.env.DATABASE_URL,	// use DATABASE_URL environment variable from Heroku app 
   ssl: {
     rejectUnauthorized: false // don't check for SSL cert
   }
-});
+}); */
 
-/* const pool = new Pool({
+const pool = new Pool({
   user: 'me',
   host: 'localhost',
   database: 'api',
   password: 'password',
   port: 5432,
-}) */
+})
 
 const getMusicals = (request, response) => {
   console.log('Get musical by key!');
